4e28397b3c765309323a98ee54b318eeba91dbc2
churchill.travis@gmail.com <churchill.travis@gmail.com@2ea354d3-2efb-c152-2a85-cc394b82f97f>
2011-03-31 16:26:56 +0000

    wip - implementing search
    git-svn-id: https://aesemployment2011.googlecode.com/svn/trunk@91 2ea354d3-2efb-c152-2a85-cc394b82f97f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-
+  protect_from_forgery    
+=begin
+this is a block comment that will not be executed
+=end
 end
--- app/controllers/positions_search_controller.rb
+++ app/controllers/positions_search_controller.rb
@@ -2,9 +2,11 @@ class PositionsSearchController < ApplicationController
   def index
     @title = "Open Position Search Controller"
     #grab a list of properties for the dropdowns
-    @locations = Position.find(:all, :select => "DISTINCT(location)").map(&:location)
+    #@locations = Position.find(:all, :select => "DISTINCT(location)").map(&:location)
     #@locations = array_from_position_attribute(location)
-    @departments = Position.find(:all, :select => "DISTINCT(department)").map(&:department)
+    #@departments = Position.find(:all, :select => "DISTINCT(department)").map(&:department)
+    @positions = Position.all
+    @position = Position.new
     #@test_text = put_some_text "Hello"
   end
 
@@ -19,3 +21,13 @@ class PositionsSearchController < ApplicationController
   def search
   end
 end
+
+=begin
+<% form.inputs :name => "Basic Search", :for => :position do |f| %>
+  <% f.inputs do %>
+    <%= f.input :title %>
+    <%= f.input :location, :collection => Position.find(:all, :select => "DISTINCT(location)").map(&:location) %>
+    <%= f.input :department, :collection => Position.find(:all, :select => "DISTINCT(department)").map(&:department) %>
+  <% end %>
+  <%= f.commit_button %>
+=end
