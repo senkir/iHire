5ce2865d7f672bd84be24d70ca6f8ec6dff150b1
churchill.travis@gmail.com <churchill.travis@gmail.com@2ea354d3-2efb-c152-2a85-cc394b82f97f>
2011-04-20 03:06:41 +0000

    general answers no longer duplicate
    git-svn-id: https://aesemployment2011.googlecode.com/svn/trunk@113 2ea354d3-2efb-c152-2a85-cc394b82f97f

--- app/helpers/general_questions_helper.rb
+++ app/helpers/general_questions_helper.rb
@@ -1,9 +1,12 @@
 module GeneralQuestionsHelper
   def build_all_answers person
     @questions = GeneralQuestion.all
-    @questions.each do | question |
-    person.general_answers.build(:general_question_id => question.id)
-    person.save
+    @answers = person.general_answers.all
+    if @answers.empty?
+      @questions.each do | question |
+        person.general_answers.build(:general_question_id => question.id)
+        person.save
+      end
     end
   end
   
